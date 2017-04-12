430aaaf3a2242a2d11d8d97ca9989b03a926612b
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-04-12 19:41:12 +0900

    Create_group＿Editing_screen
    ・Create_group＿Editing_screen

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,19 @@
 class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to :root, notice: 'チャットグループが作成されました。'
+    else
+      render new_group_path, alert: 'チャットグループの作成に失敗しました。'
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name,{ user_ids:[]})
+  end
 end
