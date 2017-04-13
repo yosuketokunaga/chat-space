6b82842171c41b92ea3b44cb260ac3dfec10a74a
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-04-13 20:24:28 +0900

    Edit groups controller
    Edit groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,6 +13,17 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if group.user_id == current_user.id
+      group.update(group_params)
+    end
+  end
+
   private
 
   def group_params
