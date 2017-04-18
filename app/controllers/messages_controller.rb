1cd3254379f11f3f1ea8a5cc0bc2705ea8a0db0a
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-04-18 20:03:39 +0900

    Describe some controller.rb
    Describe some controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,24 @@ class MessagesController < ApplicationController
 
   def index
     @group = Group.find(params[:group_id])
+    @messages = @group.messages.includes(:user)
+    @message = Message.new
+  end
+
+  def create
+    @message = current_user.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path, notice: 'メッセージが送信されました。'
+    else
+      flash.now[:alert] = 'メッセージの送信に失敗しました。'
+      render :new
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body).merge(group_id: params[:group_id])
   end
 
 end
