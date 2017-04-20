3f3a790b2a376ce512b941ee940e6b531ca35440
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-04-20 20:43:27 +0900

    correct.messages controller
    correct.messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,7 @@
 class MessagesController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
     @group = Group.find(params[:group_id])
     @messages = @group.messages.includes(:user)
@@ -7,19 +9,18 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = current_user.messages.new(message_params)
+    @message = Message.new(message_params)
     if @message.save
       redirect_to group_messages_path, notice: 'メッセージが送信されました。'
     else
-      flash.now[:alert] = 'メッセージの送信に失敗しました。'
-      render :index
+      redirect_to group_messages_path, alert: 'メッセージが送信されませんでした。'
     end
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:body).merge(group_id: params[:group_id])
+    params.require(:message).permit(:body).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 
 end
