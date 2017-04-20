1e8a6150202cae08c8694388ad1d4b75a572da0b
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-04-20 20:40:43 +0900

    Correct some blank
    Correct some blank

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root  to: "groups#index"
+  root to: 'groups#index'
 
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
