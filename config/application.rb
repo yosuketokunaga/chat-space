7da9264414d6466ba804b82d77f3ec9688db22d1
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-04-24 13:56:53 +0900

    to make error sentence to be ja
    to make error sentence to be ja

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module ChatSpace
     # -- all .rb files in that directory are automatically loaded.
 
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [:ja, :en]
 
     config.generators do |g|
       g.helper false
