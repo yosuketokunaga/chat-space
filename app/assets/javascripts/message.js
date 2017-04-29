85dcf2ed7ffcd8aa305a8ac29854d8a3c8956f99
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-04-29 14:45:06 +0900

    correct message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,22 +1,16 @@
 $(function() {
-  function buildHTML(data) {
-    var html =
-    '<div class="chat-main__body__name">' +
-        message.user.name +
-    '</div>' +
-    '<div class="chat-main__body__date">' +
-        message.created_at +
-    '</div>' +
-    '<div class="chat-message__body">' +
-        message.body +
-    '</div>'
+  function buildHTML(message) {
+    var html = (`<div class="chat-main__body">
+                  <P class=chat-main__body__name>${ message.user.name }</p>
+                  <p class=chat-main__body__date>${ message.created_at }</p>
+                  <p class=chat-main__body__message>${ message.body }</p>
+                  </div>`);
     return html;
   }
+
   $('.js-form').on('submit', function(e) {
     e.preventDefault();
-    var formData = new FormData($(this).get(0));
-    var group_id = $('.group_id').attr('value');
-    var url = '/groups/' + group_id + '/messages.json'
+    var fd = new FormData($(this).get(0));
     $.ajax({
       type: 'POST',
       url: form.attr('action'),
@@ -27,10 +21,14 @@ $(function() {
     })
     .done(function(data) {
       var html = buildHTML(data);
-      $('.messages').append(html);
+      $('.chat-main__body').append(html);
     })
     .fail(function() {
-      alert('error');
+      alert('メッセージの送信に失敗しました。');
     });
+    return false;
   });
+  $('.chat-main__body').animate({
+    scrollTop: $(document).height()
+  },1500);
 });
