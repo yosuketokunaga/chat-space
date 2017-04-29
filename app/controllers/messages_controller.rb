0c03e3f3e94b13919e6d07b1a2252acd6abbb9f4
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-04-29 16:58:32 +0900

    change single quotation
    change single quotation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,7 +11,7 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     if @message.save
       respond_to do |format|
-       format.html { redirect_to group_messages_path, notice: "メッセージが送信されました"}
+       format.html { redirect_to group_messages_path, notice: 'メッセージが送信されました'}
        format.json
       end
     else
