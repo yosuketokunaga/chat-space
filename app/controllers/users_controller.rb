6bf1f5640439aeb5e9e4edbfc1c0f1df270c6328
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-05-03 14:28:11 +0900

    Prepare for  Incremental search
    Prepare for  Incremental search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.where('name LIKE ?', "%#{params[:name]}%")
+    render json: @users
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'groups#index'
 
-  resources :groups, only: [:new, :create, :edit, :update] do
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
+
+  resources :users, only: [:index]
 end
