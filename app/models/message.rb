24c1d17d6684bbc0dd0a5e39c9aa1e71730f1631
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-05-05 16:13:46 +0900

    delete validation
    delete validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,6 @@
 class Message < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   belongs_to :user
   belongs_to :group
-
-  validates :body, presence: true
-  mount_uploader :image, ImageUploader
 end
